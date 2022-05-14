0d511cc5d7c56d34102ff4f267b2c4c9d0712296
aneeshthanikkal@gmail.com <aneeshthanikkal@gmail.com>
2022-05-15 00:34:10 +0530

    renaming

--- src/main/java/com/io/tedtalk/helper/CSVHelper.java
+++ src/main/java/com/io/tedtalk/helper/CSVHelper.java
@@ -33,20 +33,20 @@ public class CSVHelper {
 		try (BufferedReader fileReader = new BufferedReader(new InputStreamReader(is, "UTF-8"));
 				CSVParser csvParser = new CSVParser(fileReader,
 						CSVFormat.DEFAULT.withFirstRecordAsHeader().withIgnoreHeaderCase().withTrim());) {
-			List<TedTalk> tutorials = new ArrayList<TedTalk>();
+			List<TedTalk> tedTalks = new ArrayList<TedTalk>();
 			Iterable<CSVRecord> csvRecords = csvParser.getRecords();
 			Long likes=(long) 0;
 			Long views=(long) 0;
 			for (CSVRecord csvRecord : csvRecords) {
 				likes = StringUtils.isNumeric(csvRecord.get(TedTalkConstants.LIKES))?Long.parseLong(csvRecord.get(TedTalkConstants.LIKES)):0;
 				views = StringUtils.isNumeric(csvRecord.get(TedTalkConstants.VIEWS))?Long.parseLong(csvRecord.get(TedTalkConstants.VIEWS)):0;
-				TedTalk tutorial = new TedTalk(csvRecord.get(TedTalkConstants.TITLE), Instant.now(),
+				TedTalk tedTalk = new TedTalk(csvRecord.get(TedTalkConstants.TITLE), Instant.now(),
 						csvRecord.get(TedTalkConstants.AUTHOR), views,
 						likes, csvRecord.get(TedTalkConstants.LINK),
 						Generators.timeBasedGenerator().generate().toString());
-				tutorials.add(tutorial);
+				tedTalks.add(tedTalk);
 			}
-			return tutorials;
+			return tedTalks;
 		} catch (IOException e) {
 			throw new RuntimeException("fail to parse CSV file: " + e.getMessage());
 		}
