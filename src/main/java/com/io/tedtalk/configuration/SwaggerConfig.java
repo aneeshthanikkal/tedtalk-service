dff45f8ee4a9e37054f714c91899adb4c4167906
aneeshthanikkal@gmail.com <aneeshthanikkal@gmail.com>
2022-05-16 11:35:29 +0530

    jpa test added and bug fix

--- src/main/java/com/io/tedtalk/configuration/SwaggerConfig.java
+++ src/main/java/com/io/tedtalk/configuration/SwaggerConfig.java
@@ -1,7 +1,7 @@
 package com.io.tedtalk.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -13,7 +13,7 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@Component
+@EnableWebMvc
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig implements WebMvcConfigurer {
---
+++ src/test/java/com/io/tedtalk/DaoTests.java
@@ -0,0 +1,38 @@
+package com.io.tedtalk;
+
+import java.time.Instant;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import com.io.tedtalk.model.TedTalk;
+import com.io.tedtalk.repository.TedTalkRepository;
+ 
+ 
+@ExtendWith(SpringExtension.class)
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class DaoTests {
+ 
+  @Autowired
+  TedTalkRepository tedTalkRepository;
+ 
+  @Test
+  public void testCreateReadDelete() {
+    
+    TedTalk tedTalk = new TedTalk("Title", Instant.now(), "Author", 1l, 1l, "link", "id");
+ 
+    tedTalkRepository.save(tedTalk);
+ 
+    Iterable<TedTalk> tedTalks = tedTalkRepository.findAll();
+    Assertions.assertThat(tedTalks).extracting(TedTalk::getTitle).containsOnly("Title");
+ 
+    tedTalkRepository.deleteAll();
+    Assertions.assertThat(tedTalkRepository.findAll()).isEmpty();
+  }
+}
